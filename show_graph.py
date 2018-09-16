5e19eac8ee981ce005daee5ec98c2aa5ba30546d
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2018-09-16 18:14:09 -0500

    can display all available values with the -l flag

--- show_graph.py
+++ show_graph.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 # Example:
-# python3 Analysis/show_graph.py -f voo.csv,nvda.csv -p voo,nvda -v close
+# python3 show_graph.py -f voo.csv,nvda.csv -p voo,nvda -v close
 
 from matplotlib import pyplot as plt
 import pandas as pd
 import datetime
+import functools
 from optparse import OptionParser
 
 
@@ -29,6 +30,12 @@ def display(dataframes_dct, values):
     plt.show()
 
 
+def display_values(dataframes_dct):
+    all_column_headers = (df.keys() for (_, df) in dataframes_dct.items())
+    return [str(header) for header in
+                functools.reduce(lambda x, acc: x & acc, all_column_headers)]
+
+
 if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option('-f', '--files', dest='files',
@@ -40,17 +47,27 @@ if __name__ == '__main__':
     parser.add_option('-v', '--values', dest='values',
                       help='Which field values to show from csv')
 
-    parser.add_option('-d', '--display_values', action='store_true',
+    parser.add_option('-l', '--list-values', action='store_true',
                       dest='display_values',
                       help='Display available fields and exit')
 
     (options, args) = parser.parse_args()
 
+    if not options.values:
+        print('which values do you want to display? (use -l to for list)')
+        parser.print_help()
+        quit()
+
+    if not options.products:
+        products = [filename.split('.')[0] for filename in options.files]
+    else:
+        products = options.products.split(',')
+
     dataframes_dct = parse_input_files(options.files.split(','),
-                                       options.products.split(','))
+                                       products)
 
     if options.display_values:
-        print('Not implemented')
+        print(', '.join(display_values(dataframes_dct)))
         quit()
 
     display(dataframes_dct, options.values.split(','))
